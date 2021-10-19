0b2d5241bb5837ea2d6acce560599a5c78ac15fd
tangjm <jaredtang@hotmail.co.uk>
2021-10-19 15:14:58 +0100

    Decoupled Item class from checkout.spec.js and Receipt class from Checkout.js

--- spec/checkout.spec.js
+++ spec/checkout.spec.js
@@ -1,28 +1,44 @@
 const Checkout = require('../src/checkout.js')
+const Receipt = require('../src/receipt.js')
 // you should be able to delete the line below when Checkout is properly isolated
-const Item = require('../src/item.js')
 
-describe('Checkout', function() {
+// class TestItem {
+//   constructor(price) {
+//     this.price = price;
+//   }
+// }
+// let checkout = new Checkout();
+
+
+
+describe('Checkout', function () {
   let items, checkout
 
-  beforeEach(function() {
+  beforeEach(function () {
+    // items = [
+    //   new TestItem(1),
+    //   new TestItem(1),
+    //   new TestItem(1),
+    // ]
     items = [
-      new Item(1),
-      new Item(2),
-      new Item(2),
+      { price: 1 },
+      { price: 2 },
+      { price: 2 },
     ]
     checkout = new Checkout(items)
+    receiptObj = new Receipt(checkout.total());
+
   })
 
-  describe('total()', function() {
-    it('returns items total', function() {
+  describe('total()', function () {
+    it('returns items total', function () {
       expect(checkout.total()).toEqual(5)
     })
   })
 
-  describe('printReceipt()', function() {
-    it('returns items total', function() {
-      expect(checkout.printReceipt()).toEqual('Your total is 5. Thanks for shopping!')
+  describe('printReceipt()', function () {
+    it('returns items total', function () {
+      expect(checkout.printReceipt(receiptObj)).toEqual('Your total is 5. Thanks for shopping!')
     })
   })
 })
--- src/checkout.js
+++ src/checkout.js
@@ -2,18 +2,23 @@ const Receipt = require('./receipt.js')
 
 class Checkout {
   constructor(items) {
-    this.items = items
+    this.items = items;
   }
 
   total() {
+    // items = [{ price: 1}, {price: 2}, {price: 3}]
     return this.items.reduce((a, b) => a + b.price, 0)
   }
 
-  printReceipt() {
+  printReceipt(receiptObj) {
     // Move the pointer to the dependency from the function body to outside the class or as a default method argument
-    const receipt = new Receipt(this.total())
-    return receipt.print()
+    // const receipt = new Receipt(this.total())
+
+    return receiptObj.print();
   }
 }
+// old code
+// let receipt = new Receipt(this.total());
+
 
 module.exports = Checkout
